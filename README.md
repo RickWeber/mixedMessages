ed041d763395a8fff13746ea2e27364efa94e308
RickWeber <Rick.Weber05@gmail.com>
2021-10-07 10:20:39 -0400

    get script generating output on command line

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # mixedMessages
 
 This program generates random messages of despair and cruelty. Feel free to use it to automate your own anxiety so you can free up your attention for more satisfying and useful things.
+
+To use it, download the script and run `node script.js` on your terminal. Respond to the question, feel bad, shake it off, and get back to your life.
+
+Please don't take the output of this script too seriously. If the output really gets to you, please talk to someone. If you really need to, talk to the good folks at the [National Suicide Prevention Lifeline](https://suicidepreventionlifeline.org/).
--- script.js
+++ script.js
@@ -2,17 +2,50 @@
 function randInt(max){
 	return Math.floor(Math.random() * max);
 }
-
-const userPrompt = "What are you anxious about? What do you fear? Please phrase your answer in this format: 'I'm afraid that my job will keep getting worse and I'll be too afraid to leave.'";
-
+// take input
+const readline = require('readline').createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+// ask the user for something to work with
+const userPrompt = "What are you anxious about? What do you fear? Please phrase your answer in this format: 'My job will keep getting worse and I'll be too afraid to leave'\n\n\n";
+// convert first person statements to second person
 function firstToSecondPerson(phrase){
 	// convert statements like "nobody will ever love me" 
 	// to "nobody will ever love you"
 	let conversions = {
 		"i": "you",
+		"i'll": "you'll",
+		"i'd": "you'd",
+		"i'm": "you are",
 		"my": "your",
 		"mine": "yours",
 		"me": "you"
 	};
-
+	out = [];
+	for(const word of phrase.toLowerCase().split(' ')){
+		Object.keys(conversions).includes(word.toLowerCase()) ?
+			out.push(conversions[word.toLowerCase()]) :
+			out.push(word) ;
+	}
+	return out.join(' ');
 }
+// and really drive it home.
+const clinchers = [
+	' and you always will.',
+	' and nobody understands.',
+	" and there's no way around that."
+]
+// put it all together
+function processAnswer(phrase){
+	let first = "I'm here to tell you that ";
+	let middle = firstToSecondPerson(phrase);
+	let end = clinchers[randInt(clinchers.length)];
+	return first + middle + end;
+}
+// ask for fears and feed them right back
+readline.question(userPrompt, answer => {
+		console.log(processAnswer(answer));
+		readline.close();
+	}
+);
